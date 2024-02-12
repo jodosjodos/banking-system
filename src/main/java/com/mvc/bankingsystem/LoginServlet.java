42b688d4dc29c095685ce5e2ce8a54b5db159f03
jodosjodos <jeandedieu2030@gmail.com>
2024-02-12 06:34:12 +0200

    done with login

--- src/main/java/com/mvc/bankingsystem/LoginServlet.java
+++ src/main/java/com/mvc/bankingsystem/LoginServlet.java
@@ -7,11 +7,59 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.*;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String bankType = req.getParameter("bankType");
+        ResultSet resultSet = insertData(email);
+        try {
+            if (resultSet.next()) {
+                // You can access the columns of the ResultSet here
+                String retrievedEmail = resultSet.getString("email");
+                String retrievedPassword = resultSet.getString("password");
+
+                // Perform your desired logic here
+                if (email.equals(retrievedEmail) && password.equals(retrievedPassword)) {
+                    // Successful login
+                    res.sendRedirect("home.jsp");
+                } else {
+                    // Invalid email or password
+                    req.setAttribute("error", "Invalid email or password");
+                    req.getRequestDispatcher("error.jsp").forward(req, res);
+                }
+            } else {
+                // User not found
+                req.setAttribute("error", "User not found");
+                req.getRequestDispatcher("error.jsp").forward(req, res);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ResultSet insertData(String email) {
+        String url = "jdbc:postgresql://localhost:5432/work";
+        String usernameDb = "jodos";
+        String passwordDb = "jodos2006";
+
+        String sql = "SELECT * FROM  users where email=? ";
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(url, usernameDb, passwordDb);
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setString(1, email);
+
+            return st.executeQuery();
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
